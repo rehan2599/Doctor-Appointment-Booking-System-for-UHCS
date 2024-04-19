0c35dcfcea1d76af340bbb16fe42ae9e47aa1ba3
Rehan Khan <khan.reh@northeastern.edu>
2024-04-19 06:16:29 -0400

    Doctor Detail page completed

---
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import avatar from "../../assets/images/avatar-icon.png";
+export const Feedback = () => {
+  return (
+    <div>
+      <div className="mb-[50px]">
+        <h4 className="text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]">
+          All reviews (272)
+        </h4>
+
+        <div className="flex  ">
+
+        </div>
+      </div>
+    </div>
+  )
+}
