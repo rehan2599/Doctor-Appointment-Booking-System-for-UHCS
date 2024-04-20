37b42e0727d3a6609d4f7c46eb2e902b5adb885d
073021sam <158006447+saima-diwan@users.noreply.github.com>
2024-04-19 22:59:40 -0400

    DoctorCard changes

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -1,50 +1,50 @@
-import React from 'react';
-import starIcon from '../../assets/images/Star.png';
-import {Link} from 'react-router-dom';
-import {BsArrowRight,} from 'react-icons/bs';
+import React, { useState } from 'react';
+import { BASE_URL} from "..//../config";
+import DoctorCard from './../..components/Loader/Loading';
+import Error from "../../components/Error/Error";
 
-const DoctorCard = ({doctor}) => {
-    const {name, avgRating, totalRating, photo, specialization, totalPatients, hospital,} = doctor;
 
-  return (
-    <div className='p-3 lg:p-5'>
+const useFetchdata = (url) => {
+
+    const {data:appointments, loading,error } = useFetchData('${BASE_URL}')
+
+    const [data,setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState9(null)
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const res = await fetch(url, {
+                headers:{Authorization : 'Bearer $ {token}'}
+            })
+        }
+
+
+    },[])
+
+    return (
         <div>
-            <img src={photo} className='w-full' alt="" />
-        </div>
+            {loading && !error && <Loadin />}
 
-        <h2 className='text-[18px] leading-[30px] lg:text-[26px] lg:leading-9 text-headingColor font-[700] mt-3 lg:mt-5'>
-            {name}
-        </h2>
+            {error && !error && <Error errMessage={error} /> }
 
-        <div className='mt-2 lg:mt-4 flex items-center justify-between'>
-            <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-2 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded'>
-                {specialization}
-            </span>
-
-            <div className="flex items-center gap-[6px]">
-                <span className="flex items-center gap-[6px] text-[14px] leading-6 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
-                    <img src={starIcon} alt="" /> {avgRating}
-                </span> 
-                <span className='text-[14px] leading-6 lg:text-[16px] lg:leading-7 font-semibold text-headingColor'>({totalRating})</span>
+            {!loading && !error && (
+                <div className='grid grid-cols-1 lg-grid-cols-2 gap-5'>
+                    {appointments.map(doctor => (
+                        <DoctorCard doctor={doctor} key={doctor._id} />
+                    ))}
+                    </div>
+            )}
             </div>
-        </div>
-
-        <div className='mt-[18px] lg:mt-5 flex items-center justify-between'>
-            <div>
-                <h3 className='text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold text-headingColor'>
-                    +{totalPatients} patients
-                </h3>
-                <p className='text-[14px] leading-6 font-[400] text-textColor'>
-                    At {hospital}
-                </p>
+            )}
             </div>
 
-            <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
-                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
-            </Link>
-        </div>
-    </div>
-  )
-}
+            </div>
+    )}
 
-export default DoctorCard
+    {!loading && !error && appointments.length===0 &&
+         <h2 className='mt-5 text-center text-headingColor'>You did not book any doctor yet</h2>}
+};
+
+export default MyBookings;
+
