92b6e9de4cd615cd0aa6b6161f0d3ebf4f44aa6e
Rehan Khan <khan.reh@northeastern.edu>
2024-04-20 06:27:32 -0400

    User detail update functionality added

---
+++ frontend/src/components/Error/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Error = ({errMessage}) => {
+  return <div className='flex items-center justify-center w-full h0='>
+    <h3 className="text-headingColor text=[20px] leading-[30px] font-semibold">
+        {errMessage}
+    </h3>
+  </div>
+}
