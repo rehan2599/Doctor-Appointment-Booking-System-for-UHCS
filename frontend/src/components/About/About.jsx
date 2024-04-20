dd18a43b32bbb17d220b61fe8d970139cb75da6d
073021sam <158006447+saima-diwan@users.noreply.github.com>
2024-04-19 22:57:49 -0400

    About.jsx

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,46 +1,54 @@
-import React from 'react';
-import aboutImg from '../../assets/images/about.png';
-import aboutCardImg from '../../assets/images/about-card.png';
-import { Link } from 'react-router-dom';
+import React from "react" ; 
+import userImg from '../../assets/images/doctor-img01.png'
+import {authContext} from "./../../context/AuthContext";
 
-const About = () => {
-  return (
-    <section>
-        <div className='container'>
-            <div className='flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row'>
-                {/* About image */}
+import MyBookings from "./MyBookings";
+import Profile from "./Profile"
 
-                <div className='relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1'>
-                    <img src={aboutImg} alt="" />
-                    <div className='absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[22%]'>
-                        <img src={aboutCardImg} alt="" />
-                    </div>
-                </div>
-                {/* About Content */}
-                <div className='w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2'>
-                    <h2 className='heading'>
-                        Proud to be one of the nations best 
-                    </h2>
-                    <p className='text_para'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    </p>
+import useGetProfile from '../..hooks/useFetchData'
+import { BASE_URL } from "../../config";
 
-                    <p className='text_para mt-[30px]'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
+const MyAccount = () => {
+    const {dispatch} = useContext(authContext);
+    const {tab, setTab} = useState("bookings");
 
-                    <Link to='/'>
-                        <button className='btn'>
-                            Learn More
-                        </button>
-                    </Link>
-                </div>
+    const {
+        data: userData,
+        loading,
+        error,
+    } = useGetProfile('${BASE_URL}/user/profile/me');
+
+    console.log(userData, 'userdata');
+
+    const handleLogout = () => {
+        dispatch({ type : "LOGOUT"};)
+
+    }};
+
+    return (
+        <section>
+            <div className="max-w-[1170px] px-5 mx-auto">
+                
+                {loading && <Loading/>}
+
+                { error && <Error/>}
+
+                {
+                    !loading && !error && (
+                        <div className="grid md:grid-cols-3 gap-10">
+                            <div className="pb-[50px] px-[30px] rounded-md">
+                                <div className="flex items-center justify-center">
+                                    <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
+                                        <img
+                                          src={userImg}
+                                          alt=""
+                                    </figure>
+                                </div>
+                            </div>
+                    )
+                }
+
 
             </div>
-        </div>
-  </section>
-  );
- 
-}
-
-export default About
+        </section>
+    )
